1425437971d5c2dc958faf5e4c943b03a5f5e3ea
Suresh Chidella <venkatasuresh051@yahoo.com>
2024-05-29 16:32:28 +0530

    moved configserver to seperate repo

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM eclipse-temurin:17-jdk-alpine
+VOLUME /tmp
+COPY target/*.jar config-server-0.0.1.jar
+ENTRYPOINT ["java","-jar","/config-server-0.0.1.jar"]
+EXPOSE 8888
---
+++ src/test/java/com/codeCracker/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeCracker.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
